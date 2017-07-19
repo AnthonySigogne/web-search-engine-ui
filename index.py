7247e7a77810e208e6c1e2fb72a10f22ae7832b4
anthony sigogne <anthonysigogne@MBP-de-anthony.lan>
2017-07-19 17:18:41 +1100

    update;better template

--- index.py
+++ index.py
@@ -3,7 +3,8 @@
 
 """
 UI - a simple web search engine.
-The goal is to index an infinite list of URLs (web pages), and then be able to quickly search relevant URLs against a query.
+The goal is to index an infinite list of URLs (web pages),
+and then be able to quickly search relevant URLs against a query.
 
 See https://github.com/AnthonySigogne/web-search-engine for more information.
 """
@@ -16,6 +17,7 @@ __version__ = "1.0"
 
 import os
 import requests
+from urllib import parse
 from flask import Flask, request, jsonify, render_template
 
 # init flask app and env variables
@@ -48,8 +50,7 @@ def search():
             r = requests.post('http://%s:%s/search'%(host, port), data = {
                 'query':query,
                 'hits':hits,
-                'start':start,
-                'highlight':1
+                'start':start
             })
         except :
             return "Error, check your installation"
@@ -57,16 +58,88 @@ def search():
         # get data and compute range of results pages
         data = r.json()
         i = int(start/hits)
-        range_pages = range(i-5,i+5) if i >= 6 else range(0,1+int(data["total"]/hits))
+        maxi = 1+int(data["total"]/hits)
+        range_pages = range(i-5,i+5 if i+5 < maxi else maxi) if i >= 6 else range(0,maxi if maxi < 10 else 10)
 
         # show the list of matching results
-        return render_template('layout.html', query=query,
+        return render_template('spatial/index.html', query=query,
             response_time=r.elapsed.total_seconds(),
             total=data["total"],
             hits=hits,
             start=start,
             range_pages=range_pages,
-            results=data["results"])
+            results=data["results"],
+            page=i,
+            maxpage=maxi-1)
 
     # return homepage (no query)
-    return render_template('layout-empty.html')
+    return render_template('spatial/index.html')
+
+@app.route("/reference", methods=['POST'])
+def reference():
+    """
+    URL : /reference
+    Request the referencing of a website.
+    Method : POST
+    Form data :
+        - url : url to website
+        - email : contact email
+    Return homepage.
+    """
+    # POST data
+    data = dict((key, request.form.get(key)) for key in request.form.keys())
+    if not data.get("url", False) or not data.get("email", False) :
+        return "Vous n'avez pas renseigné l'URL ou votre email."
+
+    # query search engine
+    try :
+        r = requests.post('http://%s:%s/reference'%(host, port), data = {
+            'url':data["url"],
+            'email':data["email"]
+        })
+    except :
+        return "Une erreur s'est produite, veuillez réessayer ultérieurement"
+
+    return "Votre demande a bien été prise en compte et sera traitée dans les meilleurs délais."
+
+# -- JINJA CUSTOM FILTERS -- #
+
+@app.template_filter('truncate_title')
+def truncate_title(title):
+    """
+    Truncate title to fit in result format.
+    """
+    return title if len(title) <= 70 else title[:70]+"..."
+
+@app.template_filter('truncate_description')
+def truncate_description(description):
+    """
+    Truncate description to fit in result format.
+    """
+    if len(description) <= 160 :
+        return description
+
+    cut_desc = ""
+    character_counter = 0
+    for i, letter in enumerate(description) :
+        character_counter += 1
+        if character_counter > 160 :
+            if letter == ' ' :
+                return cut_desc+"..."
+            else :
+                return cut_desc.rsplit(' ',1)[0]+"..."
+        cut_desc += description[i]
+    return cut_desc
+
+@app.template_filter('truncate_url')
+def truncate_url(url):
+    """
+    Truncate url to fit in result format.
+    """
+    url = parse.unquote(url)
+    if len(url) <= 60 :
+        return url
+    url = url[:-1] if url.endswith("/") else url
+    url = url.split("//",1)[1].split("/")
+    url = "%s/.../%s"%(url[0],url[-1])
+    return url[:60]+"..." if len(url) > 60 else url
